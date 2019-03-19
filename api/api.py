b8668157fa0b9b85c4ef303b1d388c712e4cafcb
Lucas Picoloto <lfpicoloto@gmail.com>
2019-03-19 00:20:35 -0300

    change to flask

---
+++ api/api.py
@@ -0,0 +1,35 @@
+import json
+
+from flask import Flask, request, jsonify
+import grpc
+
+import requests_mercadolibre_pb2
+import requests_mercadolibre_pb2_grpc
+
+
+app = Flask(__name__)
+
+
+@app.route('/response', methods=["POST"])
+def hello_world():
+    data = request.get_json()
+    if isinstance(data, list):
+        result = run(data)
+        return jsonify(result)
+    return "Its not a list"
+
+
+def run(urls):
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = requests_mercadolibre_pb2_grpc.RequestStub(channel)
+        response = stub.RequestGo(requests_mercadolibre_pb2.ListURLs(urls=urls))
+        results = []
+        for i in response.resp:
+            print(i)
+            my_obj = json.loads(i)
+            results.append(my_obj)
+        return results
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=3000)
--- api/protobuf_requests/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProtobufRequestsConfig(AppConfig):
-    name = 'protobuf_requests'
